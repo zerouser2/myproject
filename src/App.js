3eead67f480e53b1749e37cb294efc86bdc526a9
zerouser2 <usernewuser0614@gmail.com>
2024-05-26 17:09:58 +0500

    nurikgay

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          ААААААААААа
+          NURIK GAY XAAXAXAX :D
         </p>
         <a
           className="App-link"
